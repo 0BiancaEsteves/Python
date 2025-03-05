a8530fc78e47c6ee8d78bc12a7c92ca1f57006e9
Bianca <BiEsteves2006@gmail.com>
2025-03-05 18:14:46 -0300

    iniciando python

---
+++ Python/Python desafio/desafio3.py
@@ -0,0 +1,3 @@
+n1 = input ('Diga um numero: ')
+n2 = input ('Diga outo numero: ')
+print (n1+n2)
---
+++ Python/exercício2.py
@@ -0,0 +1,3 @@
+nome = input ('Olá! Qual é seu nome?:')
+print('Prazer em conhecer,{}!'.format(nome))
+#print('Prazer em conhecer,', nome)
