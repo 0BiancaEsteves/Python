0ad2bfa1df92f3a7783f831aa071faef90248c1f
Bianca <BiEsteves2006@gmail.com>
2025-03-06 17:43:59 -0300

    Fazando lições de python (parte 1)

--- Python/Python desafio/desafio3.py
+++
@@ -1,3 +0,0 @@
-n1 = input ('Diga um numero: ')
-n2 = input ('Diga outo numero: ')
-print (n1+n2)
---
+++ Python/exercício3.py
@@ -0,0 +1,4 @@
+n1 = int(input ('Diga um numero: '))
+n2 = int(input ('Diga outro numero: '))
+S = n1 + n2
+print('O resultado de {} + {} = {}'.format (n1,n2,S))
