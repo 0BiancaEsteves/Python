fe849ad05190cd1909a40c2b20766bdec1edf8e0
Bianca Esteves <BiEsteves2006@gmail.com>
2025-03-13 14:04:55 -0300

    Rename exercício13.py to exercício013.py

--- Python/exercício13.py
+++
@@ -1,3 +0,0 @@
-salário = float(input('Qual o seu atual salário?:R$ '))
-novo = salário + (salário * 15 / 100 )
-print('Um funcionário que ganhava R${:.2f}, com o aumento de 15% vai ganhar R${:.2f}.'.format(salário, novo))
